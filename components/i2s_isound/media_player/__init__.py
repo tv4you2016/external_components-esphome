cb45862c5376c6c67cbb0b32607fbeed3e0d15f2
[PT]GOODVIBE[PT] <83761813+tv4you2016@users.noreply.github.com>
2025-06-10 10:08:09 +0100

    NEW VERSION

--- components/i2s_isound/media_player/__init__.py
+++ components/i2s_isound/media_player/__init__.py
@@ -1,28 +1,27 @@
-import esphome.codegen as cg
-from esphome.components import media_player, esp32, i2c
-import esphome.config_validation as cv
-
 from esphome import pins
-
-
-from esphome.const import CONF_ID, CONF_MODE
+import esphome.codegen as cg
+from esphome.components import esp32, media_player , i2c
+import esphome.config_validation as cv
+from esphome.const import CONF_MODE
 
 from .. import (
-    i2s_isound_ns,
-    I2SAudioComponent,
-    I2SAudioOut,
     CONF_I2S_ISOUND_ID,
     CONF_I2S_DOUT_PIN,
+    CONF_LEFT,
+    CONF_MONO,
+    CONF_RIGHT,
+    CONF_STEREO,
+    I2SAudioComponent,
+    I2SAudioOut,
+    i2s_isound_ns,
+    use_legacy,
 )
 
 CODEOWNERS = ["@jesserockz"]
 DEPENDENCIES = ["i2s_isound"]
 
 I2SAudioMediaPlayer = i2s_isound_ns.class_(
-    "I2SAudioMediaPlayer",
-    cg.Component,
-    media_player.MediaPlayer,
-    I2SAudioOut,
+    "I2SAudioMediaPlayer", cg.Component, media_player.MediaPlayer, I2SAudioOut,
     i2c.I2CDevice,
 )
 
@@ -35,12 +34,12 @@ CONF_DAC_TYPE = "dac_type"
 CONF_I2S_COMM_FMT = "i2s_comm_fmt"
 
 INTERNAL_DAC_OPTIONS = {
-    "left": i2s_dac_mode_t.I2S_DAC_CHANNEL_LEFT_EN,
-    "right": i2s_dac_mode_t.I2S_DAC_CHANNEL_RIGHT_EN,
-    "stereo": i2s_dac_mode_t.I2S_DAC_CHANNEL_BOTH_EN,
+    CONF_LEFT: i2s_dac_mode_t.I2S_DAC_CHANNEL_LEFT_EN,
+    CONF_RIGHT: i2s_dac_mode_t.I2S_DAC_CHANNEL_RIGHT_EN,
+    CONF_STEREO: i2s_dac_mode_t.I2S_DAC_CHANNEL_BOTH_EN,
 }
 
-EXTERNAL_DAC_OPTIONS = ["mono", "stereo"]
+EXTERNAL_DAC_OPTIONS = [CONF_MONO, CONF_STEREO]
 
 NO_INTERNAL_DAC_VARIANTS = [esp32.const.VARIANT_ESP32S2]
 
@@ -59,16 +58,17 @@ def validate_esp32_variant(config):
 CONFIG_SCHEMA = cv.All(
     cv.typed_schema(
         {
-            "internal": media_player.MEDIA_PLAYER_SCHEMA.extend(
+            "internal": media_player.media_player_schema(I2SAudioMediaPlayer)
+            .extend(
                 {
-                    cv.GenerateID(): cv.declare_id(I2SAudioMediaPlayer),
                     cv.GenerateID(CONF_I2S_ISOUND_ID): cv.use_id(I2SAudioComponent),
                     cv.Required(CONF_MODE): cv.enum(INTERNAL_DAC_OPTIONS, lower=True),
                 }
-            ).extend(cv.COMPONENT_SCHEMA),
-            "external": media_player.MEDIA_PLAYER_SCHEMA.extend(
+            )
+            .extend(cv.COMPONENT_SCHEMA),
+            "external": media_player.media_player_schema(I2SAudioMediaPlayer)
+            .extend(
                 {
-                    cv.GenerateID(): cv.declare_id(I2SAudioMediaPlayer),
                     cv.GenerateID(CONF_I2S_ISOUND_ID): cv.use_id(I2SAudioComponent),
                     cv.Required(
                         CONF_I2S_DOUT_PIN
@@ -92,10 +92,17 @@ CONFIG_SCHEMA = cv.All(
 )
 
 
+def _final_validate(_):
+    if not use_legacy():
+        raise cv.Invalid("I2S media player is only compatible with legacy i2s driver.")
+
+
+FINAL_VALIDATE_SCHEMA = _final_validate
+
+
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = await media_player.new_media_player(config)
     await cg.register_component(var, config)
-    await media_player.register_media_player(var, config)
     await i2c.register_i2c_device(var, config)
     await cg.register_parented(var, config[CONF_I2S_ISOUND_ID])
 
@@ -111,13 +118,11 @@ async def to_code(config):
 
     cg.add_library("WiFiClientSecure", None)
     cg.add_library("HTTPClient", None)
+    #cg.add_library("esphome/ESP32-audioI2S", "2.3.0")
     cg.add_library(
         name="ESP32-audioI2S-esphome",
         repository="https://github.com/tv4you2016/ESP32-audioI2S-esphome",
-        version="2.0.9",
-    )
-    # cg.add_library("esphome/ESP32-audioI2S", "2.0.7")
+        version="3.0.13",
+    )    
 
     cg.add_build_flag("-DAUDIO_NO_SD_FS")
-    cg.add_build_flag("-DAUDIO_LOG")
-    cg.add_build_flag("-DAUDIO_MUTEX")
