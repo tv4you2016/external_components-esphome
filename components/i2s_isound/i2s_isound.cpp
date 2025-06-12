c649102171416eaa72ef3f2a8371a6a763485708
[PT]GOODVIBE[PT] <83761813+tv4you2016@users.noreply.github.com>
2025-06-12 20:34:09 +0100

    Update i2s_isound.cpp

--- components/i2s_isound/i2s_isound.cpp
+++ components/i2s_isound/i2s_isound.cpp
@@ -9,9 +9,9 @@ namespace i2s_isound {
 
 static const char *const TAG = "i2s_isound";
 
-#if defined(USE_ESP_IDF) && (ESP_IDF_VERSION_MAJOR >= 5)
-static const uint8_t I2S_NUM_MAX = SOC_I2S_NUM;  // because IDF 5+ took this away :(
-#endif
+
+static const uint8_t I2S_NUM_MAX = SOC_I2S_NUM; 
+
 
 void I2SAudioComponent::setup() {
   static i2s_port_t next_port_num = I2S_NUM_0;
